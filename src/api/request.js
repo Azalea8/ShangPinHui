c2ea7bdb2c25dcb70a1092c5231bdfe386a3c877
Azalea <2574292235@qq.com>
2023-07-01 16:10:59 +0800

    Home组件静态页面完成，axios封装进行中...

---
+++ src/api/request.js
@@ -0,0 +1,6 @@
+// 对axios二次封装
+import axios from "axios";
+
+const request = axios.create({
+
+})
--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -2,15 +2,32 @@
   <div>
     <TypeNav></TypeNav>
     <ListContainer></ListContainer>
+    <Recommend></Recommend>
+    <Rank></Rank>
+    <Like></Like>
+    <Floor></Floor>
+    <Floor></Floor>
+    <Brand></Brand>
   </div>
 </template>
 
 <script>
 import ListContainer from "@/pages/Home/ListContainer/index.vue";
+import Recommend from "@/pages/Home/Recommend/index.vue";
+import Rank from "@/pages/Home/Rank/index.vue";
+import Like from "@/pages/Home/Like/index.vue";
+import Floor from "@/pages/Home/Floor/index.vue";
+import Brand from "@/pages/Home/Brand/index.vue";
+
 export default {
   name: 'Home',
   components: {
+    Brand,
+    Floor,
+    Like,
+    Rank,
     ListContainer,
+    Recommend,
   }
 }
 </script>
